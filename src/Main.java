6611db618a755bb46c05ed37674cd4db8809f376
Yuriy Idigeshev <id.yv1983@gmail.com>
2022-06-29 19:36:46 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 620;
+        int miles = cost / 20;
+        System.out.println("Итоговые мили:" + miles);
+    }
+}
